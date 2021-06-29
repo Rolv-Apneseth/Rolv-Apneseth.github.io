1a510a26530e8d4656c014d9c65446b718b173f2
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-06-30 00:13:46 +0100

    Added fullpage fade-in animations on scroll, only added if javascript is availaable

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-// Auto expand text area
-const textarea = document.getElementById("message");
+// Apply fullpage animations
+function fullpageAnimations() {
+  const elements = [
+    ...document.querySelectorAll(".section-header, .skill"),
+    ...document.querySelectorAll("p, form, img:not(#arrow-down)"),
+  ];
 
-textarea.addEventListener("input", () => {
-  textarea.style.height = textarea.scrollHeight + "px";
-});
+  const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -30px 0px",
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+      console.log(entry.target);
+    });
+  }, appearOptions);
+
+  elements.forEach((element) => {
+    // fade-in class only added if javascript is available
+    element.classList.add("fade-in");
+    appearOnScroll.observe(element);
+  });
+}
+
+// Auto expand text area
+function autoExpand() {
+  const textarea = document.getElementById("message");
+
+  textarea.addEventListener("input", () => {
+    textarea.style.height = textarea.scrollHeight + "px";
+  });
+}
 
 // Lazy load images and iframes
 function lazyLoad() {
@@ -32,4 +65,6 @@ function lazyLoad() {
 // ON DOM LOADED
 window.addEventListener("DOMContentLoaded", function () {
   lazyLoad();
+  autoExpand();
+  fullpageAnimations();
 });
